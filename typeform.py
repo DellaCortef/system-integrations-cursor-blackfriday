b81874075a9687b782c68570260580ad9a690bd9
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-10 18:31:14 -0300

    API request and response example

--- typeform.py
+++ typeform.py
@@ -1,2 +1,10 @@
 import requests
 
+# forms_id from our Typeform account
+forms_id = "qHJL1W2j"
+
+# API request
+response = requests.get(f"https://api.typeform.com/forms/{forms_id}/responses")
+
+# print API response
+print(response.json())
