c5acf09a8c56f4af3bced9238fecd614b4750e3b
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-10 18:35:58 -0300

    creating API_TOKEN variable in .env file

--- typeform.py
+++ typeform.py
@@ -1,10 +1,7 @@
 import requests
 
-# forms_id from our Typeform account
-forms_id = "qHJL1W2j"
+# Replace with your Typeform API token and form ID
+api_token = "API_TOKEN"
 
-# API request
-response = requests.get(f"https://api.typeform.com/forms/{forms_id}/responses")
 
-# print API response
-print(response.json())
+
