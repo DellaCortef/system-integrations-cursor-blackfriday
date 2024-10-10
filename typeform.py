4e8271229e7442334e9ef2e4c337742939169bb0
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-10 18:40:38 -0300

    building API request-> response

--- typeform.py
+++ typeform.py
@@ -2,6 +2,7 @@ import requests
 
 # Replace with your Typeform API token and form ID
 api_token = "API_TOKEN"
+form_id   = "FORM_ID"
 
 # Typeform Responses API URL
 url = f"https://api.typeform.com/forms/{form_id}/responses"
