c75bf95e07ca53b1c325c707e0dc31a7454d4532
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-10 18:38:32 -0300

    building API request-> response

--- typeform.py
+++ typeform.py
@@ -3,5 +3,10 @@ import requests
 # Replace with your Typeform API token and form ID
 api_token = "API_TOKEN"
 
+# Typeform Responses API URL
+url = f"https://api.typeform.com/forms/{form_id}/responses"
+headers = {"Authorization": f"Bearer {api_token}"}
+params = {"page_size": 100}
 
-
+# Making the request to the Typeform API
+response = requests.get(url, headers=headers, params=params)
