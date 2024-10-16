1c57ad77072b8a7017e23f1972792d9ee11711b7
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-16 08:00:45 -0300

    creating our dimensions and metrics through schemas

--- google_analytics_to_sql.py
+++ google_analytics_to_sql.py
@@ -1,3 +1,7 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import (
     DateRange,
@@ -5,6 +9,9 @@ from google.analytics.data_v1beta.types import (
     Metric,
     RunReportRequest,
 )
+
+# Load environment variables
+load_dotenv()
 
 
 def sample_run_report(property_id="YOUR-GA4-PROPERTY-ID"):
@@ -17,17 +24,54 @@ def sample_run_report(property_id="YOUR-GA4-PROPERTY-ID"):
     # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
     client = BetaAnalyticsDataClient()
 
+    property_id = os.getenv("GA_PROPERTY_ID")
+    start_date = os.getenv("GA_START_DATE")
+
     request = RunReportRequest(
         property=f"properties/{property_id}",
-        dimensions=[Dimension(name="city")],
-        metrics=[Metric(name="activeUsers")],
-        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+        dimensions=[
+            Dimension(name="date"),
+            Dimension(name="city"),
+            Dimension(name="country"),
+            Dimension(name="deviceCategory"),
+            Dimension(name="sessionSource"),
+            Dimension(name="sessionMedium"),
+        ],
+        metrics=[
+            Metric(name="totalUsers"),
+            Metric(name="newUsers"),
+            Metric(name="activeUsers"),
+            Metric(name="sessions"),
+            Metric(name="engagedSessions"),
+            Metric(name="averageSessionDuration"),
+            Metric(name="screenPageViews"),
+            Metric(name="conversions"),
+            Metric(name="totalRevenue"),
+        ],
+        date_ranges=[DateRange(start_date=start_date, end_date="today")],
     )
     response = client.run_report(request)
 
-    print("Report result:")
+   # Create lists to store data
+    data = []
+    header = [dim.name for dim in request.dimensions] + [metric.name for metric in request.metrics]
+
+    # Fill in the data list
     for row in response.rows:
-        print(row.dimension_values[0].value, row.metric_values[0].value)
+        row_data = [dim_value.value for dim_value in row.dimension_values] + [metric_value.value for metric_value in row.metric_values]
+        data.append(row_data)
+
+    # Create a DataFrame with the data
+    df = pd.DataFrame(data, columns=header)
+
+    # Configuring the connection to the PostgreSQL database
+    db_url = os.getenv("DATABASE_URL")
+    engine = create_engine(db_url)
+
+    # Save the DataFrame to the 'google_analytics_data' table in the database
+    df.to_sql(name='google_analytics_data', con=engine, if_exists='replace', index=False)
+
+    print("Dados do Google Analytics salvos no banco de dados PostgreSQL.")
 
 if __name__ == "__main__":
     sample_run_report()
